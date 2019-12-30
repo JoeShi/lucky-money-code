1f3fcc93bc00726b86679382747761feafaa2567
Joe SHI <qiaoshi@amazon.com>
2019-12-31 00:39:02 +0800

    add README

--- README.md
+++ README.md
@@ -1,16 +1,31 @@
 # Sumerian Demo
 
-## 8th Wall 
+## Run the demo and debug on Mobile
 
-You will have to register an 8th Wall account to continue this demo.
-
-
-
+1. clone this repo, `git clone git@github.com:JoeShi/sumerian-amplify-app.git`
+1. Checkout the **develop** branch, `git checkout develop`
+1. You will have to register an 8th Wall account to continue this demo
+1. Replace the `APP_KEY` with your own value in `src/index.html`
+```html
+<script async src="https://apps.8thwall.com/xrweb?appKey=APP_KEY"></script>
+```
+1. commit `src/index.html` & push to **develop** branch
+1. check Amplify Console for the auto deployment. The CI/CD has been configured
+1. Visit the [https://develop.d100ntcyk3qzf1.amplifyapp.com/](https://develop.d100ntcyk3qzf1.amplifyapp.com/) to access the demo, you will have to register an account, if you have not registered yet
 
 ## Launch the demo locally
 
-Run the following command to install the depedencies.
-```
+Install the depedencies.
+```shell
 npm install 
 ```
+
+Run locally
+```shell
+npm start
+```
+
+## Resource
+
+[Augmented Reality Using Amazon Sumerian, AWS Amplify, and 8th Wall](https://docs.sumerian.amazonaws.com/tutorials/create/intermediate/augmented-reality-using-sumerian-8thwall/)
 
--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, {Component} from 'react';
 import './App.css';
-import { withAuthenticator, SumerianScene  } from 'aws-amplify-react';
-import Amplify from 'aws-amplify';
+import { withAuthenticator } from 'aws-amplify-react';
+import Amplify, {XR as awsXR} from 'aws-amplify';
 import Aws_exports from './aws-exports';
 
 Amplify.configure(Aws_exports);
@@ -9,10 +9,36 @@ Amplify.configure(Aws_exports);
 class App extends Component {
   render() {
     return (
-      <div style={ { height: '100vh' } }>
-        <SumerianScene sceneName='ardemo'/>
+      <div id="sumerian-scene-dom-id" style={ {height: '100vh'} }>
+        <p id="loading-status">Loading...</p>
       </div>
     );
+  }
+
+  componentDidMount() {
+    this.loadAndStartScene();
+  }
+
+  async loadAndStartScene() {
+    await awsXR.loadScene('ardemo', 'sumerian-scene-dom-id');
+
+    const world = awsXR.getSceneController('ardemo').sumerianRunner.world;
+
+    window.sumerian.SystemBus.addListener('xrerror', (params) => {
+      // Add error handling here
+    });
+
+    window.sumerian.SystemBus.addListener('xrready', () => {
+      // Both the Sumerian scene and XR8 camera have loaded. Dismiss loading status
+      const loadingStatus = window.document.getElementById('loading-status');
+      if (loadingStatus && loadingStatus.parentNode) {
+        loadingStatus.parentNode.removeChild(loadingStatus);
+      }
+    });
+
+    window.XR8.Sumerian.addXRWebSystem(world);
+
+    awsXR.start('ardemo');
   }
 };
 
