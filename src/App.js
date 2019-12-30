838418c049766394481446ad734e56cca41be405
Joe SHI <qiaoshi@amazon.com>
2019-12-30 23:55:31 +0800

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,19 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
+import { withAuthenticator, SumerianScene  } from 'aws-amplify-react';
+import Amplify from 'aws-amplify';
+import Aws_exports from './aws-exports';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+Amplify.configure(Aws_exports);
 
-export default App;
+class App extends Component {
+  render() {
+    return (
+      <div style={ { height: '100vh' } }>
+        <SumerianScene sceneName='demo'/>
+      </div>
+    );
+  }
+};
+
+export default withAuthenticator(App, { includeGreetings: true });
